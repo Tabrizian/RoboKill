a2cd97c3c9fd1b4781ccd6a99c15704d4c8a193e
mmaghajani <mma137421@gmail.com>
2015-06-10 00:33:02 +0430

    Adds fields

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,6 +1,10 @@
-import javax.swing.JComponent;
+import java.util.ArrayList;
 
+import javax.swing.JComponent;
 
 public class Enemy extends JComponent {
 
+	private Position pos;
+	private ArrayList<Weapon> weapons;
+	private int health;
 }
