eaccb7a0500122b7d5d16d9e2cab579b72668baf
mmaghajani <mma137421@gmail.com>
2015-06-10 00:38:51 +0430

    Adds fields

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,15 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 
 public class Game extends JFrame {
 
+	private GameField field ;
+	private Robot robot ;
+	private JButton map ;
+	private JButton inv ;
+	private JButton menu ;
+	private JLabel cash ;
+	private JLabel shield ;
 }
