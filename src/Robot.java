5c7fbe6cbcbe8178897b15eb54464bc0dadd203a
mmaghajani <mma137421@gmail.com>
2015-06-10 00:27:01 +0430

    Adds fields

--- src/Robot.java
+++ src/Robot.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+
 import javax.swing.JComponent;
 
 
 public class Robot extends JComponent {
 
+	private Position pos ;
+	private ArrayList<Weapon> weapons ;
+	private int health ;
 }
