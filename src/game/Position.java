71a92afe44f9cd41f8901d727ac2bb9c86ab4f7c
mmaghajani <mma137421@gmail.com>
2015-06-12 19:15:41 +0430

    Add some methods

--- src/game/Position.java
+++ src/game/Position.java
@@ -1,7 +1,76 @@
 package game;
 
+/**
+ * This is position class that contain position of component in a frame
+ * 
+ * @author mahdi
+ *
+ */
 public class Position {
 
-	private int x ;
-	private int y ;
+	/**
+	 * X Position
+	 */
+	private int x;
+	/**
+	 * Y Position
+	 */
+	private int y;
+
+	public Position() {
+
+	}
+
+	public Position(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	/**
+	 * Decrease X position if X be greater than zero
+	 */
+	public void decX() {
+		if (x > 1)
+			x--;
+	}
+
+	/**
+	 * Increase X position without condition
+	 */
+	public void incX() {
+		x++;
+	}
+
+	/**
+	 * Increase X position if X be lower than a limit
+	 * @param limit
+	 */
+	public void incX(int limit) {
+		if (x < limit)
+			x++;
+	}
+	
+	/**
+	 * Decrease Y position if Y be greater than zero
+	 */
+	public void decY() {
+		if (y > 1)
+			y--;
+	}
+
+	/**
+	 * Increase Y position without condition
+	 */
+	public void incY() {
+		y++;
+	}
+
+	/**
+	 * Increase Y position if Y be lower than a limit
+	 * @param limit
+	 */
+	public void incY(int limit) {
+		if (y < limit)
+			y++;
+	}
 }
