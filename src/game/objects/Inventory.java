7a4680c989b1c252e7f8e6be198e1b6e99bd69af
Iman Tabrizian <tabrizian@outlook.com>
2015-06-23 02:06:57 +0430

    Improvement in Inventory class.

--- src/game/objects/Inventory.java
+++ src/game/objects/Inventory.java
@@ -2,7 +2,27 @@ package game.objects;
 
 import java.util.ArrayList;
 
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
 public class Inventory {
 	private ArrayList<AddOne> addOnes;
+	private Image inventoryImage;
+	private String inventoryAddress;
+	
+	public void init(){
+		for (AddOne addOne : addOnes) {
+			try {
+				addOne.imageInInventory = new Image(addOne.imageInInventoryAddress);
+			} catch (SlickException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void add(AddOne addOne){
+		addOnes.add(addOne);
+	}
 	
 }
