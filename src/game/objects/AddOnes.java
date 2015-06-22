3e38b128e8687cfbee2763a6eb950e3522474a52
Iman Tabrizian <tabrizian@outlook.com>
2015-06-23 01:28:31 +0430

    Inits image of AddOne.

--- src/game/objects/AddOnes.java
+++ src/game/objects/AddOnes.java
@@ -1,7 +1,21 @@
 package game.objects;
 
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
 public class AddOnes {
 	
-	protected String imageAddress;
+	protected String imageInInventory;
+	private Image img; 
+	
+	
+	public void init(){
+		try {
+			img = new Image(imageInInventory);
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 }
