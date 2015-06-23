5e8f37f2c9adc17e58d4808f1efe37921d64500e
mmaghajani <mma137421@gmail.com>
2015-06-23 23:16:01 +0430

    Animate robot motion

--- src/game/objects/Robot.java
+++ src/game/objects/Robot.java
@@ -8,9 +8,12 @@ import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import org.newdawn.slick.Animation;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
+import org.newdawn.slick.InputListener;
+import org.newdawn.slick.KeyListener;
 import org.newdawn.slick.SlickException;
 
 /**
@@ -55,6 +58,14 @@ public class Robot {
 	 * Place for add a gun that ordered from left to right
 	 */
 	private int[] places = { 0, 0, 0, 0 };
+
+	private Animation toUp;
+	private boolean iskeyUpPressed = false;
+
+	private Animation fixState;
+
+	private Animation toDown;
+	private boolean iskeyDownPressed = false;
 
 	public Robot() {
 		super();
@@ -125,6 +136,9 @@ public class Robot {
 			e.printStackTrace();
 		}
 
+		toUp = new Animation(createToUpFrames(), 150);
+		fixState = new Animation(createFixStateFrame(), 150);
+		toDown = new Animation(createToDownFrames(), 150) ;
 	}
 
 	/**
@@ -133,7 +147,7 @@ public class Robot {
 	 * @param angle
 	 */
 	public void draw() {
-		imageOfLeg.drawCentered(pos.getX(), pos.getY());
+
 		imageOfBody.setRotation(imageAngleDeg);
 		imageOfBody.drawCentered(pos.getX(), pos.getY());
 
@@ -141,6 +155,42 @@ public class Robot {
 			gun.draw();
 
 		}
+
+		if (iskeyUpPressed == true) {
+
+			fixState.stop();
+			toUp.draw(pos.getX() - imageOfBody.getWidth() / 2, pos.getY()
+					- imageOfBody.getHeight() / 2);
+			toUp.start();
+			imageOfBody.setRotation(imageAngleDeg);
+			imageOfBody.drawCentered(pos.getX(), pos.getY());
+		}
+
+	/*	else if (iskeyDownPressed == true) {
+
+			fixState.stop();
+			toDown.draw(pos.getX() - imageOfBody.getWidth() / 2, pos.getY()
+					- imageOfBody.getHeight() / 2);
+			toDown.start();
+			imageOfBody.setRotation(imageAngleDeg);
+			imageOfBody.drawCentered(pos.getX(), pos.getY());
+		}
+		*/
+		if (iskeyUpPressed == false) {
+			toUp.stop();
+			fixState.draw(pos.getX() - imageOfBody.getWidth() / 2, pos.getY()
+					- imageOfBody.getHeight() / 2);
+			imageOfBody.setRotation(imageAngleDeg);
+			imageOfBody.drawCentered(pos.getX(), pos.getY());
+		}
+/*
+		else if (iskeyDownPressed == false) {
+			toDown.stop();
+			fixState.draw(pos.getX() - imageOfBody.getWidth() / 2, pos.getY()
+					- imageOfBody.getHeight() / 2);
+			imageOfBody.setRotation(imageAngleDeg);
+			imageOfBody.drawCentered(pos.getX(), pos.getY());
+		}*/
 
 	}
 
@@ -156,10 +206,14 @@ public class Robot {
 		Input input = gc.getInput();
 		if (input.isKeyDown(Input.KEY_UP) || input.isKeyDown(input.KEY_W)) {
 			yPos -= 0.25;
-		}
+			iskeyUpPressed = true;
+		} else
+			iskeyUpPressed = false;
 		if (input.isKeyDown(Input.KEY_DOWN) || input.isKeyDown(input.KEY_S)) {
 			yPos += 0.25;
-		}
+			iskeyDownPressed = true;
+		} else
+			iskeyDownPressed = false;
 		if (input.isKeyDown(Input.KEY_RIGHT) || input.isKeyDown(input.KEY_D)) {
 			xPos += 0.25;
 		}
@@ -188,9 +242,9 @@ public class Robot {
 	 * Fires missiles
 	 */
 	public void fire() {
-		
+
 		for (Weapon gun : weapons) {
-			gun.shot(imageAngleRad, pos, imageOfBody.getWidth() * 70 / 100 );
+			gun.shot(imageAngleRad, pos, imageOfBody.getWidth() * 70 / 100);
 		}
 	}
 
@@ -222,5 +276,105 @@ public class Robot {
 	}
 
 	public void remGun() {
+	}
+
+	private Image[] createToUpFrames() {
+		Image[] frames = new Image[5];
+
+		try {
+			Image image1 = new Image("pics/robot/image 123.png");
+			frames[0] = image1;
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			Image image2 = new Image("pics/robot/image 231.png");
+			frames[1] = image2;
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			Image image3 = new Image("pics/robot/image 485.png");
+			frames[2] = image3;
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			Image image4 = new Image("pics/robot/image 653.png");
+			frames[3] = image4;
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			Image image5 = new Image("pics/robot/image 589.png");
+			frames[4] = image5;
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return frames;
+	}
+
+	private Image[] createFixStateFrame() {
+		Image[] frames = new Image[1];
+
+		try {
+			Image image1 = new Image("pics/robot/image 123.png");
+			frames[0] = image1;
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return frames;
+	}
+
+	private Image[] createToDownFrames() {
+
+		Image[] frames = new Image[5];
+
+		try {
+			Image image1 = new Image("pics/robot/image 123.png");
+			frames[4] = image1;
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			Image image2 = new Image("pics/robot/image 231.png");
+			frames[3] = image2;
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			Image image3 = new Image("pics/robot/image 485.png");
+			frames[2] = image3;
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			Image image4 = new Image("pics/robot/image 653.png");
+			frames[1] = image4;
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			Image image5 = new Image("pics/robot/image 589.png");
+			frames[0] = image5;
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return frames;
+
 	}
 }
