6237fda0bd4f34610539763dcea710294e3cd54a
mmaghajani <mma137421@gmail.com>
2015-06-25 13:48:50 +0430

    Implements Map class

--- src/game/Map.java
+++ src/game/Map.java
@@ -1,5 +1,39 @@
 package game;
 
+import game.objects.Robot;
+
 public class Map {
 
+	/**
+	 * Rooms
+	 */
+	private GameField[][] fields ;
+	/**
+	 * The robot
+	 */
+	private Robot robot ;
+	/**
+	 * Player who controls robot
+	 */
+	private Player player ;
+	
+	public Map(){
+		
+	}
+	
+	public void init(){
+		
+	}
+	
+	public void draw(){
+		
+	}
+	
+	public void update(){
+		
+	}
+	
+	public void drawInScopeMode(){
+		
+	}
 }
