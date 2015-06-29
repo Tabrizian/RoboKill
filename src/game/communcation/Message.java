55642ac1294a5d495ac6646ef9d9313b2a7887e1
Iman Tabrizian <tabrizian@outlook.com>
2015-06-30 01:04:14 +0430

    Adds sample implementation of message class.

--- src/game/communcation/Message.java
+++ src/game/communcation/Message.java
@@ -1,5 +1,11 @@
 package game.communcation;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Message {
 	public static final String INIT = "init"; 
 	public static final String UPDATE = "update";
@@ -9,6 +15,19 @@ public class Message {
 	public Message(String text, String header){
 		this.text = text;
 		this.header = header;
+	}
+	
+	public void send(Socket client){
+		OutputStream out = null;
+		try {
+			out = client.getOutputStream();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		PrintWriter pw = new PrintWriter(out);
+		pw.println(text);
+		pw.println(header);
 	}
 	
 	
