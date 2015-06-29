2d087c1e636787736e44cd778235411a9702fe02
Iman Tabrizian <tabrizian@outlook.com>
2015-06-30 01:16:02 +0430

    Adds sample implementation of listener.

--- src/game/communcation/Listener.java
+++ src/game/communcation/Listener.java
@@ -1,5 +1,36 @@
 package game.communcation;
 
-public class Listener implements Runnable{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Vector;
+
+public class Listener implements Runnable {
+
+	private Socket socket;
+	private Vector<Message> messages;
+
+	public Listener(Socket socket) {
+		this.socket = socket;
+		messages = new Vector<Message>();
+	}
+
+	@Override
+	public void run() {
+		try {
+			InputStream in = socket.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			while (true) {
+				String header = br.readLine();
+				String text = br.readLine();
+				messages.add(new Message(text, header));
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 }
