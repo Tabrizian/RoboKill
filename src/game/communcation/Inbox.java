884f8b8d02c654bdf8e96846713572396b8a2c0c
mmaghajani <mma137421@gmail.com>
2015-06-30 01:25:01 +0430

    Implements Inbox class

--- src/game/communcation/Inbox.java
+++ src/game/communcation/Inbox.java
@@ -1,5 +1,35 @@
 package game.communcation;
 
+import java.util.ArrayList;
+
 public class Inbox {
 
+	private ArrayList<Message> messages ;
+	private static Inbox instance ;
+	
+	private Inbox(){
+		
+	}
+	
+	public static Inbox	getInbox(){
+		if( instance == null )
+			instance = new Inbox() ;
+		return instance ;
+	}
+	
+	public void addMessage( Message m ){
+		messages.add(m) ;
+	}
+	
+	public void delMessage( Message m ){
+		messages.remove(m) ;
+	}
+	
+	public void delMessage( int index ){
+		messages.remove(index) ;
+	}
+	
+	public ArrayList<Message> getMessages(){
+		return messages ;
+	}
 }
